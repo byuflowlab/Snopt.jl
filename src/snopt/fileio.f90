82e13242b5873bdcd96343b751ad520e55c666d6
Andrew Ning <aning@byu.edu>
2017-02-02 23:05:50 -0700

    fix fileio file

--- src/snopt/fileio.f90
+++ src/snopt/fileio.f90
@@ -1,50 +1,24 @@
-subroutine openfiles(printnum, printerr)
+subroutine openfiles(printnum, sumnum, printerr, sumerr)
 
     ! inputs
-    integer, intent(in) :: printnum
+    integer, intent(in) :: printnum, sumnum
 
     ! outputs
-    integer, intent(out) :: printerr
+    integer, intent(out) :: printerr, sumerr
 
     open(printnum, file='snopt-print.out', action='write', status='replace', iostat=printerr)
+    open(sumnum, file='snopt-summary.out', action='write', status='replace', iostat=sumerr)
+
 
 end subroutine
 
 
-subroutine closefiles(printnum)
+subroutine closefiles(printnum, sumnum)
 
     ! inputs
-    integer, intent(in) :: printnum
+    integer, intent(in) :: printnum, sumnum
 
     close(printnum)
+    close(sumnum)
 
 end subroutine
-
-
-
-
-! subroutine openfiles(printnum, sumnum, printerr, sumerr)
-!
-!     ! inputs
-!     integer, intent(in) :: printnum, sumnum
-!
-!     ! outputs
-!     integer, intent(out) :: printerr, sumerr
-!
-!     open(printnum, file='snopt-print.txt', action='write', status='replace', iostat=printerr)
-!     open(sumnum, file='snopt-summary.txt', action='write', status='replace', iostat=sumerr)
-!
-!
-! end subroutine
-!
-!
-! subroutine closefiles(printnum, sumnum)
-!
-!     ! inputs
-!     integer, intent(in) :: printnum, sumnum
-!
-!     close(printnum)
-!     close(sumnum)
-!
-! end subroutine
-!
